9f5ef4c1ce7614bce56e9d64a6dcb54a48def85f
ilukianov <ilukianov@cogniance.com>
2016-03-20 18:33:17 +0200

    Added active tab

--- src/js/base.controller.js
+++ src/js/base.controller.js
@@ -1,4 +1,9 @@
 angular.module('base')
 .controller('baseCtrl', ['PAGES', '$scope', function(PAGES, $scope){
+    $scope.number = 0;
+
+    $scope.setPage = function setPage (i) {
+        $scope.number = i;
+    };
     $scope.PAGES = PAGES;
 }]);
