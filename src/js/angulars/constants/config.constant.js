c48183de3af9c4e16c4d223a0a9dd858d53edb97
ilukianov <ilukianov@cogniance.com>
2016-04-05 17:41:21 +0300

    Moved config to base module

--- src/js/angulars/constants/config.constant.js
+++ src/js/angulars/constants/config.constant.js
@@ -1,6 +1,6 @@
 angular.module('config')
 .constant('CONFIG', {
     TEMPLATES_URL: 'pages',
-    BASE_SERVER_URL: 'http://localhost:8080/', //http://bs-mafiaclub.rhcloud.com/
+    BASE_SERVER_URL: 'http://bs-mafiaclub.rhcloud.com/', //http://bs-mafiaclub.rhcloud.com/
     LOGIN_URL: 'login'
 });
--- src/js/angulars/modules/base.module.js
+++ src/js/angulars/modules/base.module.js
@@ -1,1 +1,18 @@
-angular.module('base', ['ui.router', 'server', 'ngAnimate', 'templates']);
+angular.module('base', ['ui.router', 'server', 'ngAnimate', 'templates'],
+  ['CONFIG', 'PAGES', '$stateProvider', '$urlRouterProvider',
+  function(CONFIG, PAGES, $stateProvider, $urlRouterProvider) {
+    var tmplsUrl = CONFIG.TEMPLATES_URL;
+    $urlRouterProvider.otherwise('/' + PAGES[0].url);
+
+    for (var i = 0; i < PAGES.length; i++) {
+      var page = PAGES[i];
+      var url = '/' + page.url;
+
+      // var name = page.name;
+      $stateProvider.state(url, {
+        url: '/' + page.url,
+        templateUrl: tmplsUrl + url + '.html'
+      });
+    }
+  }
+]);
