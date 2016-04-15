4b1fb8921b49fd79942f10cf83cec8a17a455cc1
ilukianov <ilukianov@cogniance.com>
2016-04-15 13:17:12 +0300

    Refactored base controller and server service to handle data in service and attach it to scope in controller

--- src/js/angulars/controllers/base.controller.js
+++ src/js/angulars/controllers/base.controller.js
@@ -4,8 +4,8 @@ angular.module('base')
 function(PAGES, $scope, serverService, $timeout, $window, $location) {
 
     var pageUrl = $location.path().slice(1);
-    var page = findPageByUrl(pageUrl) || PAGES[0];
-    setPage(page);
+    var firstPage = findPageByUrl(pageUrl) || PAGES[0];
+    setPage(firstPage);
     $scope.PAGES = PAGES;
     $scope.isOrg = false;
     $scope.user = serverService.player;
@@ -16,20 +16,23 @@ function(PAGES, $scope, serverService, $timeout, $window, $location) {
 
     // ===== public methods
     function login (user) {
+        console.log('[base.controller] login()', arguments);
+
         serverService.$_login(user);
     }
 
     function setPage (page) {
-        console.log('[baseCtrl] setPage()', arguments);
-        $scope.page = page;
+        console.log('[base.controller] setPage()', arguments);
 
+        $scope.page = page;
         if (page.needData) {
-            fetchDataFor(page, page.needMemberLevel)
-            .then(handleData.bind(this, page));
+            fetchDataFor(page, page.needMemberLevel);
         }
     }
 
     function openNewTab (url) {
+        console.log('[base.controller] openNewTab()', arguments);
+
         if (url) {
             $window.open(url, '_blank');
         }
@@ -39,37 +42,22 @@ function(PAGES, $scope, serverService, $timeout, $window, $location) {
 
     function fetchDataFor (page, needMemberLevel) {
         console.log('[base.controller] fetchDataFor()', arguments);
-        return serverService.$_fetchData(page.url, needMemberLevel)
-            .catch(handleError.bind(this, page));
+
+        return serverService.$_fetchData(page, needMemberLevel)
+            .catch(handleError.bind(this, page))
+            .then(attchDataToScope.bind(this, $scope, page));
     }
 
-    function handleData (page, response) {
-        console.log('[base.controller] handleData()', arguments);
-        if (!response) {
-            return;
-        }
-
-        if (response.data.errorText) {
-            angular.element(document.getElementById('view'))
-                .css('visibility', 'hidden');
-            alert(response.data.errorText);
-            $location.path(setPage(PAGES[0]));
-        } else if(response.data.succesText) {
-            alert(response.data.succesText);
-        } else {
-            $scope[page.url] = response.data;
-            angular.element(document.getElementById('view'))
-                .css('visibility', 'visible');
-        }
-    }
 
     function handleError (page, err) {
         console.log('[base.controller] handleError()', arguments);
+
         $scope.err = err;
     }
 
     function findPageByUrl(url) {
         console.log('[base.controller] findPageByUrl()', arguments);
+
         for (var i = 0; i < PAGES.length; i++) {
             var el = PAGES[i];
             if (el.url == url) {
@@ -78,4 +66,13 @@ function(PAGES, $scope, serverService, $timeout, $window, $location) {
         }
     }
 
+    function attchDataToScope ($scope, page, response) {
+        console.log('[base.controller] attchDataToScope()', arguments);
+
+        if (response.errorText) {
+            $location.path(setPage(PAGES[0]));
+        } else {
+            $scope[page.url] = response;
+        }
+    }
 }]);
--- src/js/angulars/services/server.service.js
+++ src/js/angulars/services/server.service.js
@@ -18,19 +18,22 @@ angular.module('server')
     this.player.data = $cookies.getObject('player-data') || this.player.data;
 
     console.log('this.player.data', this.player.data);
-    serverService.prototype.$_fetchData = function(pageUrl, needMemberLevel) {
+    serverService.prototype.$_fetchData = function(page, needMemberLevel) {
         console.log('[server.service] $_fetchData()', arguments);
-        return $http.post(CONFIG.BASE_SERVER_URL + pageUrl,
+
+
+        return $http.post(CONFIG.BASE_SERVER_URL + page.url,
             needMemberLevel ? {
                 user: this.player.data.nick,
                 password: this.player.data.password
             } : '')
-        .catch(failCallback.bind(this, needMemberLevel));
+        .catch(failCallback.bind(this, needMemberLevel))
+        .then(handleData.bind(this, page));
     };
 
     serverService.prototype.$_login = function() {
         console.log('[server.service] $_login()', arguments);
-        console.log('this.player.data', this.player.data);
+
         return $http.post(CONFIG.BASE_SERVER_URL + CONFIG.LOGIN_URL, {
                 user: this.player.data.nick,
                 password: this.player.data.password
@@ -40,35 +43,58 @@ angular.module('server')
     };
 
     serverService.prototype.setPlayers = function(players) {
+        console.log('[server.service] setPlayers()', players);
+
         return $http.post(CONFIG.BASE_SERVER_URL + CONFIG.SET_PLAYERS_URL, {
                 user: this.player.data.nick,
                 password: this.player.data.password,
                 players: players
             })
             .catch(failCallback.bind(this))
-            .then(handleLogin.bind(this));
+            .then(handleData.bind(this, 'players'));
     };
 
     // ========== PRIVATE METHODS
     function failCallback (needMemberLevel, err) {
-        console.log('[server.service.js] failCallback()', err);
-        if (err) {
-            var obj = {
-                status: err.status,
-                text: err.statusText
-            };
-            return obj;
+        console.log('[server.service] failCallback()', err);
+        if (err && err.status === -1) {
+            alert('Сервер недоступен. Проверьте интернет соединение и сообщите администратору');
         }
     }
 
     function handleLogin (response) {
         console.log('[server.service] handleLogin()', arguments);
+
         var data  = response.data;
         if (data.errorText) {
             alert(data.errorText);
         } else {
             this.player.data = data;
             $cookies.putObject('player-data', this.player.data);
+        }
+    }
+
+
+
+    function handleData (page, response) {
+        console.log('[base.controller] handleData()', arguments);
+
+        if (!response) {
+            return;
+        }
+        if (response.data.errorText) {
+            angular.element(document.getElementById('view'))
+                .css('visibility', 'hidden');
+            alert(response.data.errorText);
+            return {
+                errorText: response.data.errorText
+            };
+        } else if(response.data.succesText) {
+            alert(response.data.succesText);
+        } else {
+            angular.element(document.getElementById('view'))
+                .css('visibility', 'visible');
+            return response.data;
         }
     }
 }]);
